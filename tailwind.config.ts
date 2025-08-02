dc60c4a9d5c28dfd5a6816e7a193c2a291fe3d29
mfmanca2000 <mfmanca2000@hotmail.com>
2025-08-02 02:10:46 +0200

    fix tailwind build issue

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,8 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  darkMode: ["class"],
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  darkMode: ["class", "dark"],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
